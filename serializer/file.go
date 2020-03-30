739124d3fd1a6a96dec3bda02d0f95ffcb40a37e
ovasquezbrito <ovasquezcyt@gmail.com>
2020-03-29 21:47:53 -0400

    Serializar mensaje protobuf

---
+++ serializer/file.go
@@ -0,0 +1,50 @@
+package serializer
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func WriteProtobufToJSONFile(message proto.Message, filename string) error {
+	data, err := ProtobufToJSON(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to JSON %v", err)
+	}
+
+	err = ioutil.WriteFile(filename, []byte(data), 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write json data to file %v", err)
+	}
+
+	return nil
+}
+
+func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to binary %v", err)
+	}
+
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write binary data to file %v", err)
+	}
+
+	return nil
+}
+
+func ReadProtobufToBinaryFile(filename string, message proto.Message) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("cannot read binary data from file: %v", err)
+	}
+
+	err = proto.Unmarshal(data, message)
+	if err != nil {
+		return fmt.Errorf("cannot unmarshal binary to proto message: %v", err)
+	}
+
+	return nil
+}
